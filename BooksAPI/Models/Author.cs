b31535a011c922a5b5a53d2cfcc7a39473995a13
Chenyan <yan.chen08@outlook.com>
2019-04-19 22:28:13 +0800

    Add BooksAPI Project

---
+++ BooksAPI/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksAPI.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        [Required]//数据验证  System.ComponentModel.DataAnnotations;
+        public string Name { get; set; }
+    }
+}
